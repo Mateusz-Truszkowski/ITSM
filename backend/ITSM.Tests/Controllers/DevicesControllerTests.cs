8c58a1293e35cc1b424d91db0b87f6fb08ccc85a
Mateusz <mateusz.truszkowski002@gmail.com>
2025-04-10 11:51:36 +0200

    Add unit tests for services controller, remove unused imports

--- backend/ITSM.Tests/Controllers/DevicesControllerTests.cs
+++ backend/ITSM.Tests/Controllers/DevicesControllerTests.cs
@@ -8,11 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ITSM.Tests.Controllers
 {
---
+++ backend/ITSM.Tests/Controllers/ServicesControllerTests.cs
@@ -0,0 +1,115 @@
+﻿using AutoMapper;
+using ITSM.Controllers;
+using ITSM.Dto;
+using ITSM.Entity;
+using ITSM.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+
+namespace ITSM.Tests.Controllers
+{
+    public class ServicesControllerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly ServicesController _controller;
+        private readonly Mock<IServicesService> _servicesServiceMock;
+        private readonly Mock<ILogger<ServicesController>> _loggerMock;
+
+        private ServiceDto testService;
+
+        public ServicesControllerTests()
+        {
+            _mapper = GetMapper();
+            _servicesServiceMock = new Mock<IServicesService>();
+            _loggerMock = new Mock<ILogger<ServicesController>>();
+            _controller = new ServicesController(_loggerMock.Object, _servicesServiceMock.Object);
+
+            testService = _mapper.Map<ServiceDto>(TestUtil.TestData.CreateTestService());
+        }
+
+        private IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Service, ServiceDto>();
+                cfg.CreateMap<ServiceDto, Service>();
+            });
+            return config.CreateMapper();
+        }
+
+        [Fact]
+        public void Get_ReturnsListOfServices()
+        {
+            _servicesServiceMock.Setup(s => s.GetServices()).Returns(new List<ServiceDto> { testService });
+
+            var result = _controller.Get();
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var services = Assert.IsAssignableFrom<List<ServiceDto>>(okResult.Value);
+            Assert.Single(services);
+            Assert.Equal(JsonConvert.SerializeObject(new List<ServiceDto> { testService }), JsonConvert.SerializeObject(services));
+        }
+
+        [Fact]
+        public void GetById_ReturnsService_WhenServiceExists()
+        {
+            _servicesServiceMock.Setup(s => s.GetService(1)).Returns(testService);
+
+            var result = _controller.Get(1);
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var service = Assert.IsAssignableFrom<ServiceDto>(okResult.Value);
+            Assert.Equal(JsonConvert.SerializeObject(testService), JsonConvert.SerializeObject(service));
+        }
+
+        [Fact]
+        public void GetById_ReturnsNotFound_WhenServiceDoesNotExist()
+        {
+            var result = _controller.Get(1);
+
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public void Post_ReturnsCreatedService()
+        {
+            _servicesServiceMock.Setup(s => s.CreateService(testService)).Returns(testService);
+
+            var result = _controller.Post(testService);
+
+            var okResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var service = Assert.IsAssignableFrom<ServiceDto>(okResult.Value);
+            Assert.Equal(JsonConvert.SerializeObject(testService), JsonConvert.SerializeObject(service));
+        }
+
+        [Fact]
+        public void Patch_ReturnsUpdatedService_WhenServiceExists()
+        {
+            _servicesServiceMock.Setup(s => s.UpdateService(testService)).Returns(testService);
+
+            var result = _controller.Patch(testService);
+
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var service = Assert.IsAssignableFrom<ServiceDto>(okResult.Value);
+            Assert.Equal(JsonConvert.SerializeObject(testService), JsonConvert.SerializeObject(service));
+        }
+
+        [Fact]
+        public void Patch_ReturnsNotFound_WhenServiceDoesNotExist()
+        {
+            var result = _controller.Patch(testService);
+
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public void Delete_ReturnsNoContent()
+        {
+            var result = _controller.Delete(1);
+
+            Assert.IsType<NoContentResult>(result);
+        }
+    }
+}
