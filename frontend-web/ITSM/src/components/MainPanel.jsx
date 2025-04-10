9e1626f3f87b693cd8ebef30e0d2c03b80a34bc2
Jakub Suski <rajsus16a@gmail.com>
2025-04-10 12:24:50 +0200

    Handled a scenario where a user tries to open MainPanel when not logged in

--- frontend-web/ITSM/src/components/MainPanel.jsx
+++ frontend-web/ITSM/src/components/MainPanel.jsx
@@ -9,73 +9,90 @@ import laptop from "../assets/icons/laptop-icon.png";
 
 function MainPanel() {
   const location = useLocation();
+  const token = localStorage.getItem("authToken");
 
   return (
     <>
-      <div className="dashboard">
-        <div className="navigation">
-          <div className="nav-logo-div">
-            <img className="nav-logo" src={rocket} alt="" />
+      {token !== null ? (
+        <div className="dashboard">
+          <div className="navigation">
+            <div className="nav-logo-div">
+              <img className="nav-logo" src={rocket} alt="" />
+            </div>
+            <div className="tabs">
+              <Link
+                to="/tickets"
+                className={`tab ${
+                  location.pathname === "/tickets"
+                    ? "tickets active"
+                    : "tickets"
+                }`}
+              >
+                <button className="tab">
+                  <div className="tab-image">
+                    <img src={ticket} alt="" />
+                  </div>
+                  <span className="tab-text">Tickets</span>
+                </button>
+              </Link>
+              <Link
+                to="/users"
+                className={`tab ${
+                  location.pathname === "/users" ? "users active" : "users"
+                }`}
+              >
+                <button className="tab">
+                  <div className="tab-image">
+                    <img src={person} alt="" />
+                  </div>
+                  <span className="tab-text">Users</span>
+                </button>
+              </Link>
+              <Link
+                to="/services"
+                className={`tab ${
+                  location.pathname === "/services"
+                    ? "services active"
+                    : "services"
+                }`}
+              >
+                <button className="tab">
+                  <div className="tab-image">
+                    <img src={cog} alt="" />
+                  </div>
+                  <span className="tab-text">Services</span>
+                </button>
+              </Link>
+              <Link
+                to="/devices"
+                className={`tab ${
+                  location.pathname === "/devices"
+                    ? "devices active"
+                    : "devices"
+                }`}
+              >
+                <button className="tab">
+                  <div className="tab-image">
+                    <img src={laptop} alt="" />
+                  </div>
+                  <span className="tab-text">Devices</span>
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="tabs">
-            <Link
-              to="/tickets"
-              className={`tab ${
-                location.pathname === "/tickets" ? "tickets active" : "tickets"
-              }`}
-            >
-              <button className="tab">
-                <div className="tab-image">
-                  <img src={ticket} alt="" />
-                </div>
-                <span className="tab-text">Tickets</span>
-              </button>
-            </Link>
-            <Link
-              to="/users"
-              className={`tab ${
-                location.pathname === "/users" ? "users active" : "users"
-              }`}
-            >
-              <button className="tab">
-                <div className="tab-image">
-                  <img src={person} alt="" />
-                </div>
-                <span className="tab-text">Users</span>
-              </button>
-            </Link>
-            <Link
-              to="/services"
-              className={`tab ${
-                location.pathname === "/services"
-                  ? "services active"
-                  : "services"
-              }`}
-            >
-              <button className="tab">
-                <div className="tab-image">
-                  <img src={cog} alt="" />
-                </div>
-                <span className="tab-text">Services</span>
-              </button>
-            </Link>
-            <Link
-              to="/devices"
-              className={`tab ${
-                location.pathname === "/devices" ? "devices active" : "devices"
-              }`}
-            >
-              <button className="tab">
-                <div className="tab-image">
-                  <img src={laptop} alt="" />
-                </div>
-                <span className="tab-text">Devices</span>
-              </button>
-            </Link>
-          </div>
+          <div className="content"></div>
         </div>
-        <div className="content"></div>
-      </div>
+      ) : (
+        <div class="unathorized">
+          <p className="unauthorized-message">NO ACCESS</p>
+          <p className="unauthorized-login">
+            PLEASE{" "}
+            <Link to="/login">
+              <span className="message-login">LOG IN</span>
+            </Link>
+          </p>
+        </div>
+      )}
     </>
   );
 }
